dac0592564063fd413b02b8544b4345c611daeae
Hatsumi <mail.sycamore587@passinbox.com>
2026-05-24 19:36:26 +0800

    chore: update personal contact information

--- data/contacts.ts
+++ data/contacts.ts
@@ -1,5 +1,5 @@
 export const contacts = [
-  { label: 'Blog', href: 'https://blog.kirishima.dev/' },
-  { label: 'Email', href: 'mailto:hello@kirishima.dev' },
-  { label: 'GitHub', href: 'https://github.com/' },
-]
+  { label: "Blog", href: "https://blog.kirishima.dev" },
+  { label: "Email", href: "mailto:mail.sycamore587@passinbox.com" },
+  { label: "GitHub", href: "https://github.com/Kirish1ma" },
+];
